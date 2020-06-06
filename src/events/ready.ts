936586c481ab14e99b17d0e7202adc3f3b9966f8
Henry Bersey <henry@bersey.com>
2020-06-06 22:54:01 +0100

    Basic Bot with Events

---
+++ src/events/ready.ts
@@ -0,0 +1,11 @@
+import IEvent from "../IEvent";
+import {Client} from "discord.js";
+
+const ReadyEvent: IEvent = {
+    name: "ready",
+    execute: (client: Client) => {
+        console.log(`${client.user?.username || "Bot"} Loaded!`)
+    }
+}
+
+export default ReadyEvent
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello")
+import dotenv from "dotenv";
+import {Client, ClientEvents} from "discord.js"
+import fs from "fs"
+import path from "path";
+import IEvent from "./IEvent";
+
+dotenv.config();
+
+const client = new Client()
+
+/**
+ * Register Events
+ */
+const registerEvents = async () => {
+    const dir = await fs.readdirSync(path.join(__dirname, "./events"))
+    for (const file of dir) {
+        const event: IEvent = (await import(path.join(__dirname, "events", file))).default
+        client.on(event.name as keyof ClientEvents, event.execute.bind(null, client))
+    }
+}
+
+/**
+ * Main
+ */
+(async () => {
+    await registerEvents()
+    await client.login()
+})();
